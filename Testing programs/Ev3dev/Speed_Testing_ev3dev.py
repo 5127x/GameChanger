99e43ace2527bdc9236236550cca41e21538bcf7
0livia-L <mynameisolivialee@gmail.com>
2020-08-11 18:44:19 +1000

    . . .

---
+++ Testing programs/Ev3dev/Speed_Testing_ev3dev.py
@@ -0,0 +1,15 @@
+#11.8.20 Speed_Testing_ev3dev.py -- > Speed_Testing_micropython.py
+
+#!/usr/bin/env python3
+from ev3dev2.motor import MoveSteering, LargeMotor,OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor import INPUT_2
+colourLeft = ColorSensor(INPUT_2)
+largeMotor_Left= LargeMotor(OUTPUT_B)
+largeMotor_Right= LargeMotor(OUTPUT_C)
+
+def straight_line(speed_value):
+    while True:
+        steering_drive.on(speed=speed_value,steering = 0)
+
+straight_line(speed_value = 5)
