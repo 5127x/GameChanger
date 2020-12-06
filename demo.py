51dea21c695a60d1211cb83972530c548427699a
Phoebe Heynemand <forestsilverpelt@gmail.com>
2020-12-06 12:58:30 +1100

    ColourAttachment_values rewritten
    - framework now used a version of colourAttachment_values that gives the option to use previously set values for the different colour keys
    - added an extra reset gyro into the framework
    - comments need to be redone again

--- demo.py
+++ demo.py
@@ -3,19 +3,20 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import ColorSensor, GyroSensor, Motor
 from pybricks.parameters import Port, Button
 from pybricks.media.ev3dev import SoundFile
-from Functions_Completed.waiting import waiting
+#from Functions_Completed.waiting import waiting
 import os
 from sys import stderr
 import time 
+
 largeMotor_Right = Motor(Port.B)
 largeMotor_Left = Motor(Port.C)
 panel = Motor(Port.D)
 print("Motors Connected", file = stderr)
 
-gyro = GyroSensor(Port.S1)
+gyro = GyroSensor(Port.S4)
 colourRight = ColorSensor(Port.S2)
 colourLeft = ColorSensor(Port.S3)
-colourkey = ColorSensor(Port.S4)
+colourkey = ColorSensor(Port.S1)
 print("Sensors Connected", file = stderr)
 
 
@@ -88,22 +89,21 @@ def colourAttachment_values():
         if Button.DOWN in ev3.buttons.pressed():
             ValueFile = open("ValueFile.txt","r")
 
-            white = ValueFile.readline([0])
-            yellow = ValueFile.readline([1])
-            red = ValueFile.readline([2])
-            blue = ValueFile.readline([3])
-            green = ValueFile.readline([4])
+            white = ValueFile.readline()
+            yellow = ValueFile.readline()
+            red = ValueFile.readline()
+            blue = ValueFile.readline()
+            green = ValueFile.readline()
 
             ValueFile.close()
             break
 
         elif Button.CENTER in ev3.buttons.pressed():
+            time.sleep(.5)
+
             # collect the white value 
-            # ev3.speaker.say('Insert white')
-            
             print('Insert white')
             ev3.screen.print('Insert white')
-            ev3.speaker.play_file(SoundFile.WHITE)
             while True:
                 if Button.CENTER in ev3.buttons.pressed():
                     white = colourkey.rgb()
@@ -112,10 +112,8 @@ def colourAttachment_values():
             time.sleep(.5)
 
             # collect the yellow value 
-            #ev3.speaker.say('Insert yellow')
             print('Insert yellow')
             ev3.screen.print('Insert yellow')
-            ev3.speaker.play_file(SoundFile.YELLOW)
             while True:
                 if Button.CENTER in ev3.buttons.pressed():
                     yellow = colourkey.rgb()
@@ -124,10 +122,8 @@ def colourAttachment_values():
             time.sleep(.5)
 
             # collect the red value 
-            # #ev3.speaker.say('Insert red')
             print('Insert red')
             ev3.screen.print('Insert red')
-            ev3.speaker.play_file(SoundFile.RED)
             while True:
                 if Button.CENTER in ev3.buttons.pressed():
                     red = colourkey.rgb()
@@ -136,10 +132,8 @@ def colourAttachment_values():
             time.sleep(.5)
 
             # collect the blue value 
-            #ev3.speaker.say('Insert blue')
             print('Insert blue')
             ev3.screen.print('Insert blue')
-            ev3.speaker.play_file(SoundFile.BLUE)
             while True:
                 if Button.CENTER in ev3.buttons.pressed():
                     blue = colourkey.rgb()
@@ -148,10 +142,8 @@ def colourAttachment_values():
             time.sleep(.5)
 
             # collect the green value 
-            #ev3.speaker.say('Insert green')
             print('Insert green')
             ev3.screen.print('Insert green')
-            ev3.speaker.play_file(SoundFile.GREEN)
             while True:
                 if Button.CENTER in ev3.buttons.pressed():
                     green = colourkey.rgb()
@@ -160,22 +152,91 @@ def colourAttachment_values():
             # finished 
             print('Finished!')
             ev3.screen.print('Finshed')
-            ev3.speaker.play_file(SoundFile.GO)
             time.sleep(1)
             print("")
 
             # return the values for the different keys
             
             ValueFile = open("ValueFile.txt","w")
-            ValueFile.write(white) 
-            ValueFile.write(yellow) 
-            ValueFile.write(red) 
-            ValueFile.write(blue) 
-            ValueFile.write(green) 
+            ValueFile.write(str(white[0])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(white[1])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(white[2])) 
+            ValueFile.write("\n")
+
+            ValueFile.write(str(yellow[0])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(yellow[1])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(yellow[2]))  
+            ValueFile.write("\n")
+
+            ValueFile.write(str(red[0])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(red[1])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(red[2])) 
+            ValueFile.write("\n")
+            
+            ValueFile.write(str(blue[0])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(blue[1])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(blue[2])) 
+            ValueFile.write("\n")
+
+            ValueFile.write(str(green[0])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(green[1])) 
+            ValueFile.write(", ")
+            ValueFile.write(str(green[2])) 
+
             ValueFile.close()
             break 
     attachment_values = [white, yellow, red, blue, green]
+
     return attachment_values
 
 
-colourAttachment_values()
+print(colourAttachment_values())
+
+ValueFile = open("ValueFile.txt","r")
+
+Vwhite = ValueFile.readline()
+Vyellow = ValueFile.readline()
+Vred = ValueFile.readline()
+Vblue = ValueFile.readline()
+Vgreen = ValueFile.readline()
+
+ValueFile.close()
+
+l = Vwhite.split(",")
+white = []
+for i in l:
+    white.append(int(i))
+print(white)
+
+l = Vyellow.split(",")
+yellow = []
+for i in l:
+    yellow.append(int(i))
+print(yellow)
+
+l = Vred.split(",")
+red = []
+for i in l:
+    red.append(int(i))
+print(red)
+
+l = Vblue.split(",")
+blue = []
+for i in l:
+    blue.append(int(i))
+print(blue)
+
+l = Vgreen.split(",")
+green = []
+for i in l:
+    green.append(int(i))
+print(green)
