46997b244676c5e91ef176fd6ea07385ba82893f
mrmic33 <55372967+0livia-L@users.noreply.github.com>
2021-06-06 12:45:00 +1000

    Blue Run started  & function for comment

--- Functions_Completed/comment.py
+++ Functions_Completed/comment.py
@@ -24,3 +24,46 @@ ev3 = EV3Brick()
 robot = DriveBase(largeMotor_Left, largeMotor_Right, wheel_diameter=62, axle_track=104)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+#!/usr/bin/env pybricks-micropython
+# - Micropython -
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import ColorSensor, Motor, GyroSensor
+from pybricks.parameters import Port, Color
+from pybricks.robotics import DriveBase
+
+# basic imports 
+import os
+from sys import stderr
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# define motors, sensors and the brick
+largeMotor_Right = Motor(Port.B)
+largeMotor_Left = Motor(Port.C)
+panel = Motor(Port.D)
+
+gyro = GyroSensor(Port.S4)
+colourRight = ColorSensor(Port.S2)
+colourLeft = ColorSensor(Port.S3)
+colourkey = ColorSensor(Port.S1)
+
+ev3 = EV3Brick()
+robot = DriveBase(largeMotor_Left, largeMotor_Right, wheel_diameter=62, axle_track=104)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# wait until the key is removed at the end of a run
+def comment(stop, threadKey, comment):
+    # log the function starting 
+    print(comment)
+
+    # read the environment variable 'is_complete'
+    is_complete = None
+    if 'IS_COMPLETE' in os.environ:
+        is_complete = int(os.environ['IS_COMPLETE'])
+
+    # log leaving the function
+    # change 'is_complete' to the threadKey so the framework knows the function is complete
+    is_complete = threadKey
+    os.environ['IS_COMPLETE'] = str(is_complete)
+
+#stopProcessing = False
+#do_nothing(lambda:stopProcessing, 0)
